5f8e648be1179a3191349ce2f63e6b91dffdb646
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-09 22:54:26 -0300

    feat: cria Arquivo personalissimo para as rotas

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Categories from './pages/Categories'
+
+const Rotas = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/categories" element={<Categories />} />
+  </Routes>
+)
+
+export default Rotas
