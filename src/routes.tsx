d6f999287d53ba438e64ab6752bd08b2db37972f
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-16 15:44:14 -0300

    feat: cria nova rota Checkout

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Categories from './pages/Categories'
 import Product from './pages/Product'
+import Checkout from './pages/Checkout'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
     <Route path="/product/:id" element={<Product />} />
+    <Route path="/checkout" element={<Checkout />} />
   </Routes>
 )
 
