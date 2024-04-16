0e452b13cea60f00ef133633de252a188e17d716
Tallyson Freitas <tallyson.eu@outlook.com>
2024-04-16 15:45:38 -0300

    style: estiliza componente checkout

---
+++ src/pages/Checkout/style.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type RowProps = {
+  marginTop?: string
+}
+
+type TabButtonProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<RowProps>`
+  display: flex;
+  column-gap: 24px;
+  margin-top: ${(props) => props.marginTop || 'auto'};
+  align-items: flex-end;
+`
+
+export const InputGroup = styled.div<InputGroupProps>`
+  flex: auto;
+
+  max-width: ${(props) => props.maxWidth || 'auto'};
+
+  label {
+    font-size: 14px;
+    margin-bottom: 8px;
+    display: block;
+  }
+
+  input,
+  select {
+    background-color: ${cores.branco};
+    height: 32px;
+    padding: 0 8px;
+    border: 1px solid ${cores.branco};
+    width: 100%;
+  }
+`
+
+export const TabButton = styled.button<TabButtonProps>`
+  cursor: pointer;
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${cores.branco};
+  background-color: ${(props) => (props.isActive ? cores.verde : cores.preto)};
+  height: 32px;
+  border: none;
+  padding: 0 8px;
+  margin-right: 16px;
+
+  img {
+    margin-right: 8px;
+  }
+`
